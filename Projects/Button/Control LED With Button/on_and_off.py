acdf0522c5653a35d5867f5bd0e4c73fe82e8989
Matias :D <matias.tajan@gmail.com>
2025-05-01 20:46:05 -0400

    Update on_and_off.py

--- Projects/Button/Control LED With Button/on_and_off.py
+++ Projects/Button/Control LED With Button/on_and_off.py
@@ -1,1 +1,11 @@
+from gpiozero import Button, LED
+import time
 
+button = Button(2)
+led = LED(17)
+
+while True:
+        button.wait_for_press()
+        print("THE BUTTON WAS PRESSED!")
+        led.toggle()
+        time.sleep(0.5)

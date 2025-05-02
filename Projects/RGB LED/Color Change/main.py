7f30ab28ae10ae02de1968f576b3d4c245145c57
Matias :D <matias.tajan@gmail.com>
2025-05-01 20:52:08 -0400

    Update main.py

--- Projects/RGB LED/Color Change/main.py
+++ Projects/RGB LED/Color Change/main.py
@@ -1,1 +1,13 @@
+from gpiozero import RGBLED 
+import time
 
+rgb_led = RGBLED(22, 17, 27)
+
+for r in range(2):
+    for g in range(2):
+        for b in range(2):
+            rgb_led.color = (r, g, b)
+            time.sleep(0.50)
+
+time.sleep(0.25)
+rgb_led.off()

390a199e0ea8a5e1e315a96ae3aa3f153bddefe7
Matias :D <matias.tajan@gmail.com>
2025-05-01 20:55:15 -0400

    Create one_servo.py

---
+++ Projects/Servo/Servo Dance/one_servo.py
@@ -0,0 +1,12 @@
+from gpiozero import AngularServo
+import time
+
+servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
+
+while (True):
+    servo.angle = 90
+    time.sleep(2)
+    servo.angle = 0
+    time.sleep(2)
+    servo.angle = -90
+    time.sleep(2)
